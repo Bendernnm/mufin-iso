e851116538653ca7fa5f09dec57ac545c7048995
bender <bender.d1mon@gmail.com>
2024-10-17 01:17:52 +0300

    add script for parsing data from datahub

---
+++ scripts/parse.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const inputFile = './datahub-iso4217.json';
+const outputFile = './iso4217.json';
+
+const inputData = require(inputFile);
+
+(() => {
+    console.log('Start here');
+    const codes = new Set();
+
+    const outputData = inputData.reduce((acc, item) => {
+        if (codes.has(item.AlphabeticCode)) {
+            return acc;
+        }
+
+        codes.add(item.AlphabeticCode);
+
+        acc.push({
+            currency: item.Currency,
+            code: item.AlphabeticCode,
+            number: item.NumericCode,
+            minorUnits: item.MinorUnit,
+        });
+
+        return acc;
+    }, []);
+
+    fs.writeFileSync(outputFile, JSON.stringify(outputData));
+})();
+
