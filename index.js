a4bbb4562964b8cba35dddf85848a9a4b97e4fbe
bender <bender.d1mon@gmail.com>
2024-10-17 01:43:13 +0300

    add root import

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = {};
+const getCI = require('./src/get-ci');
+
+module.exports = {
+  getCI,
+  getCIBy: {
+    code: (code) => getCI('code', code),
+    name: (name) => getCI('name', name),
+    number: (number) => getCI('number', number),
+  },
+};
