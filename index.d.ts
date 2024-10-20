ebc4698fc8c7f424566eb8c2f5c64c3371c8d930
bender <bender.d1mon@gmail.com>
2024-10-20 23:32:02 +0300

    add types file

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+interface CurrencyItem {
+    name: string;
+    code: string;
+    number: number;
+    minorUnits: number;
+}
+
+// Import the type for getCI
+declare function getCI(type: 'code' | 'name' | 'number', value: string): CurrencyItem;
+
+// Define the module export types
+declare const moduleExports: {
+    getCI: typeof getCI;
+    getCIBy: {
+        code: (code: string) => CurrencyItem;
+        name: (name: string) => CurrencyItem;
+        number: (number: string) => CurrencyItem;
+    };
+};
+
+export = moduleExports;
